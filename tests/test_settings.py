963399c761a017f3cc1d52a3a730a6681f5c5d3a
Jason Frerich <jasonfrerich@yahoo.com>
2019-01-14 16:16:42 -0600

    Fix failing tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,6 @@
-import lib.settings
+from getrealty import settings
 
 
 def test_hash_global_setup():
     '''simple test to check hash_global setup correctly'''
-    assert lib.settings.hash_global['cache'] == {}
+    assert settings.hash_global['cache'] == {}
