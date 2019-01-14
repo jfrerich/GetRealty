529b0b290c54720af42f25a07aafdb2bb4ba9e42
Jason Frerich <jasonfrerich@yahoo.com>
2019-01-14 11:36:57 -0600

    initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='getrealty',
+      version='1.1',
+      description='''python engine for property information from appraisal
+      sites and storing in sqlite db''',
+      url='http://github.com/jfrerich/getrealty',
+      author='Jason Frerich',
+      author_email='',
+      license='MIT',
+      packages=['getrealty'],
+      zip_safe=False)
